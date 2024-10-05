b1350ee79e5afd57cb7e6dad896fd402e7b2d19e
Sam McNair <mcnairsa@gmail.com>
2024-10-05 22:18:46 +0100

    Update main.js with minor changes

--- main.js
+++ main.js
@@ -1,47 +1,4 @@
-
-const prompt = require('prompt-sync')({sigint: true});
-
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
-
-class Field {
-  constructor(fieldArray){
-    this._fieldArray = fieldArray;
-  }
-
-// Propmt the usrer for a move
-// Make sure the move is possible
-// E.g.: 'You can go down or right' ... in the starting position
-
-// const prompt = require('prompt-sync')();
-
-  print () {
-// More sensibly we wouldn't assume the array size but would have a for loop
-    console.log(this._fieldArray[0].join(' '));
-    console.log(this._fieldArray[1].join(' '));
-    console.log(this._fieldArray[2].join(' '));
-    console.log(this._fieldArray[3].join(' '));  
-    }
-}
-
-// Test it
-const gameFieldOne = [
-  [pathCharacter, fieldCharacter, fieldCharacter],
-  [pathCharacter, fieldCharacter, hole],
-  [hole, fieldCharacter, fieldCharacter],
-  [hole, fieldCharacter, fieldCharacter],[fieldCharacter, hat, hole],
-    ];
-
-const myFieldOne = new Field(gameFieldOne);
-myFieldOne.print();
-
-const userName = prompt('What is your name Bob? ');
-console.log(`Yo, ${userName}! How's it hangin'?`)
-
-const playerMove = prompt('Please enter a move :');
-const prompt = require('prompt-sync')({sigint: true});
+let prompt = require('prompt-sync')({sigint: true});
 
 const hat = '^';
 const hole = 'O';
@@ -94,11 +51,14 @@ console.log(`Hello, ${userName}!`)
 // Game loop
 
 let playerMove;
+let i = 0;
+let j = 0;
+let playerPosition = [i, j];
 let gameOver = false;
 do {
    (playerMove = prompt('Please enter a move, u[p], d[own] l[eft] or r[ight] or "X" to quit: '));
 //   console.log(`You chose: ${playerMove}`);
-} while (!gameOver && playerMove !== 'X')
+
 // up
 // down
 
@@ -107,13 +67,30 @@ do {
 // up and down
   switch (playerMove){
     case 'u':
-      innerIndex --;
+      i--;
       break;
-      if (innerIndex < 0  || innerIndex > innerArray.length ||outerIndex < 0  || outerIndex > outerArray.length) {
+    case 'd':
+      i++;
+      break;
+    case 'l':
+      j--;
+      break;
+    case 'r':
+      j++;
+      break;
+    case 'X':
+      gameOver = true;
+      break;
+    default:
+    console.log('Not a legal input...')
+  }
+   if (i < 0  || i > 3 || j < 0  || j > 2) {
         gameOver;
       }
-
-  }
+prompt = '';
+  
 // decrement or increment the index of the outer array
 // if up or left and index < 0 - GAME OVER!
 // if down or right and index > <ARRAY>.length - GAME OVER!
+
+} while (!gameOver && playerMove !== 'X')
