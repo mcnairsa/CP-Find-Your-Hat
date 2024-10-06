679c3eb43c07f0fb04a2f0d0be50d0b22014662d
Sam McNair <mcnairsa@gmail.com>
2024-10-06 21:33:23 +0100

    Update main.js with some more minor changes

--- main.js
+++ main.js
@@ -5,17 +5,22 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
+
+// Create the game board
+const gameFieldOne = [
+  [pathCharacter, fieldCharacter, fieldCharacter],
+  [pathCharacter, fieldCharacter, hole],
+  [hole, fieldCharacter, fieldCharacter],
+  [hole, fieldCharacter, fieldCharacter],[fieldCharacter, hat, hole],
+    ];
+
+// Define class Field
 class Field {
   constructor(fieldArray){
     this._fieldArray = fieldArray;
   }
-
-// Propmt the usrer for a move
-// Make sure the move is possible
-// E.g.: 'You can go down or right' ... in the starting position
-
-// const prompt = require('prompt-sync')();
-
+// Print method of class Field
+//    prints the current state of the game board
   print () {
 // More sensibly we wouldn't assume the array size but would have a for loop
     console.log(this._fieldArray[0].join(' '));
@@ -25,24 +30,22 @@ class Field {
     }
 }
 
-// Test it
-const gameFieldOne = [
-  [pathCharacter, fieldCharacter, fieldCharacter],
-  [pathCharacter, fieldCharacter, hole],
-  [hole, fieldCharacter, fieldCharacter],
-  [hole, fieldCharacter, fieldCharacter],[fieldCharacter, hat, hole],
-    ];
-
+// Instantiate the Field object
 const myFieldOne = new Field(gameFieldOne);
 myFieldOne.print();
 
 // Get user's name and greet
-const userName = prompt('What is your name? ');
-console.log(`Hello, ${userName}!`)
+// const userName = prompt('What is your name? ');
+// console.log(`Hello, ${userName}!`)
+
+
+// Propmt the user for a move
+// Make sure the move is possible
+// E.g.: 'You can go down or right' ... in the starting position
 
 // Get player move and process
 
-/* Do until either
+/* Do until- either
 1. win
 2. player falls down a hole
 3. player falls off the edge of the board
@@ -84,10 +87,11 @@ do {
     default:
     console.log('Not a legal input...')
   }
-   if (i < 0  || i > 3 || j < 0  || j > 2) {
+    if (i < 0  || i > 3 || j < 0  || j > 2) {	
+        console.log('Sorry you screwed up. Goodbye.');
         gameOver;
       }
-prompt = '';
+// prompt = '';
   
 // decrement or increment the index of the outer array
 // if up or left and index < 0 - GAME OVER!
